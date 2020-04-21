bddcb7a57f2898aa3c3b185e8e3ec75cdcb06f19
jamesncm123 <jamesncm123@gmail.com>
2020-04-21 22:58:47 +0700

    payment

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
+app.get('/callback', function (req, res) {
  return res.send('pong');
+});
+
+app.post('/scb/payment/confirm', async (req, res) => {
+  const body = (req, ['body']);
+  console.log(body)
+  res.send(body);
 });
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-//import QRCode from'qrcode.react';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+import './App.css';
+import QRCode from'qrcode.react';
+
+
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      url:""
+    }
+  }
+  payment(){
+    fetch('https://api-sandbox.partners.scb/partners/sandbox/v1/oauth/token',{
+      method:"POST",
+      headers:{
+        'Access-Control-Allow-Headers': '*',
+        'Content-Type': 'application/json',
+        'requestUId': 'd7e992f3-c9f1-4071-8a4a-6c5839c8d317',
+        'resourceOwnerId': 'l7a0bed42f65814e82a1ca23ab8eda0e88'
+      },
+      body: {
+        "applicationKey" : "l7a0bed42f65814e82a1ca23ab8eda0e88",
+        "applicationSecret" : "b777b20368df4cdf96fba31663cabb02"
+     }
+    }).then(response=>{
+      console.log(response)
+      if(response.status.code === 1000){
+        fetch('https://api-sandbox.partners.scb/partners/sandbox/v2/deeplink/transactions',{
+          method:"POST",
+          headers:{
+            'Access-Control-Allow-Headers': '*',
+            'authorization': 'Bearer '+ response.data.accessToken,
+            'resourceOwnerId': 'l7a0bed42f65814e82a1ca23ab8eda0e88',
+            'requestUId':'d7e992f3-c9f1-4071-8a4a-6c5839c8d317',
+            'channel':'scbeasy',
+            'Content-Type': 'application/json'
+          },
+          body: {
+            "paymentAmount": "100",
+              "transactionType": "PAYMENT",
+              "transactionSubType": "BPA",
+              "ref1": "123123",
+              "ref2": "12421123",
+              "ref3": "SCB",
+              "accountTo": "382574431459828"
+          }
+        }).then(res =>{
+          console.log(res)
+          if(res.status.code === 1000){
+            this.setState({url:res.data.deeplinkUrl})
+          }
+        })
+      }
+      
+    }).catch(err=>console.log(err))
+    
+  }
+  render() {
+    return (
+      <div>
+
+      <button onClick={this.payment}>CLICK PAYMANT</button>
+      <h1>{this.state.url}</h1>
+        <QRCode
+        value={this.state.url}
+    size={256}
+         />
+      </div>
+    )
+  }
+}
